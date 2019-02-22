830cb4891e4492d8a78125519a9d6c47f4cd2afc
Jeroen Ooms <jeroenooms@gmail.com>
2019-02-22 22:50:16 +0100

    deploy gcc 8.3.0

--- ci-build.sh
+++ ci-build.sh
@@ -40,11 +40,23 @@ rm -f /mingw32/lib/*.dll.a
 rm -f /mingw64/lib/*.dll.a
 export PKG_CONFIG="/${MINGW_INSTALLS}/bin/pkg-config --static"
 
+case ${MINGW_INSTALLS} in
+mingw32)
+  dl_arch=i686
+;;
+mingw64)
+  dl_arch=x86_64
+;;
+esac
+
 for package in "${packages[@]}"; do
-    execute 'Building binary' makepkg-mingw --noconfirm --noprogressbar --skippgpcheck --nocheck --syncdeps --rmdeps --cleanbuild
-    execute 'Building source' makepkg --noconfirm --noprogressbar --skippgpcheck --allsource --config '/etc/makepkg_mingw64.conf'
+    execute 'Downloading binary' curl -OL "https://ftp.opencpu.org/gcc830/mingw-w64-${dl_arch}-gcc-8.3.0-9300-any.pkg.tar.xz"
+    execute 'Downloading binary' curl -OL "https://ftp.opencpu.org/gcc830/mingw-w64-${dl_arch}-gcc-fortran-8.3.0-9300-any.pkg.tar.xz"
+    execute 'Downloading binary' curl -OL "https://ftp.opencpu.org/gcc830/mingw-w64-gcc-8.3.0-9300.src.tar.gz"
+    #execute 'Building binary' makepkg-mingw --noconfirm --noprogressbar --skippgpcheck --nocheck --syncdeps --rmdeps --cleanbuild
+    #execute 'Building source' makepkg --noconfirm --noprogressbar --skippgpcheck --allsource --config '/etc/makepkg_mingw64.conf'
     execute 'Installing' yes:pacman --noprogressbar --upgrade *.pkg.tar.xz
-    execute 'Checking Binaries' find ./pkg -regex ".*\.\(exe\|dll\|a\|pc\)"
+    #execute 'Checking Binaries' find ./pkg -regex ".*\.\(exe\|dll\|a\|pc\)"
     deploy_enabled && mv "${package}"/*.pkg.tar.xz artifacts
     deploy_enabled && mv "${package}"/*.src.tar.gz sourcepkg
     unset package
