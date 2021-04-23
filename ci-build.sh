97ed74327867b17321fb07ff593891c71db2e7e6
Jeroen Ooms <jeroenooms@gmail.com>
2021-04-23 19:51:45 +0200

    update CI things

--- ci-build.sh
+++ ci-build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
-# AppVeyor and Drone Continuous Integration for MSYS2
-# Authors: Renato Silva, Qian Hong, Jeroen Ooms
-
-# Setup git and CI
 cd "$(dirname "$0")"
 source 'ci-library.sh'
-deploy_enabled && mkdir artifacts
-deploy_enabled && mkdir sourcepkg
+mkdir artifacts
+mkdir sourcepkg
 
 ## Remove packages
 #deploy_enabled && cd artifacts
@@ -15,27 +10,22 @@ deploy_enabled && mkdir sourcepkg
 #success 'Package removal successful'
 #exit 0
 
-# Depending on if this is an rtools40 or msys64 installation:
+# Remove preinstalled libraries depending on if this is an rtools40 or msys64 install:
 if [[ $(cygpath -m /) == *"rtools40"* ]]; then
-	# rtools40: enable upstream msys2 (but keep rtools-base as primary)
-	echo "Found preinstalled rtools40 compilers!"
+  echo "Found preinstalled rtools40 compilers!"
 else
-	# msys64: remove preinstalled toolchains
-    pacman --noconfirm -Rcsu $(pacman -Qqe | grep "^mingw-w64-")
-    pacman --noconfirm -Rcsu gcc pkg-config
+  pacman --noconfirm -Rcsu $(pacman -Qqe | grep "^mingw-w64-")
 fi
 
-# Temp hack for weird msys2 flag
-sed -i 's/,--default-image-base-high//' /etc/makepkg_mingw.conf
-
-# Enable upstream msys2 repo
+# Disable upstream mingw-packages
 cp -f pacman.conf /etc/pacman.conf
 pacman --noconfirm -Scc
 pacman --noconfirm -Syyu
-pacman --noconfirm --needed -S git base-devel binutils
+pacman --noconfirm --needed -S git base-devel binutils unzip
+pacman --noconfirm --needed -S mingw-w64-${MINGW_TOOLCHAIN}-{toolchain,libtre,pkg-config,xz}
 
-# Install core build stuff
-pacman --noconfirm --needed -S mingw-w64-{i686,x86_64}-{crt,winpthreads,gcc,libtre,pkg-config,xz}
+# Remove weird upstream build flags
+sed -i 's/,--default-image-base-high//' /etc/makepkg_mingw.conf
 
 # Initiate git
 git_config user.email 'ci@msys2.org'
@@ -73,7 +63,7 @@ for package in "${packages[@]}"; do
 done
 
 # Prepare for deploy
-deploy_enabled && cd artifacts || success 'All packages built successfully'
+cd artifacts || success 'All packages built successfully'
 execute 'Updating pacman repository index' create_pacman_repository "${PACMAN_REPOSITORY:-ci-build}"
 execute 'Generating build references'  create_build_references  "${PACMAN_REPOSITORY:-ci-build}"
 execute 'SHA-256 checksums' sha256sum *
